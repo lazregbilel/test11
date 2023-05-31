3a984f33c82489ff3bcf64b8fe5453a29466d1df
Bilel Lazreg <Bilel.lazreg@uni5-technologies.com>
2023-05-31 14:51:28 +0100

    Initial commit

---
+++ GameOfHangma/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace GameOfHangma
+{
+    public class Hangman
+    {
+        public static List<string> MatchWords = new List<string>();
+        List<string> matchWords = new List<string>();
+
+    }
+    private static  bool matchwords(string word, string pattern)
+    {
+        StreamReader words = new StreamReader("C:\\Users\\BilelUni5\\Desktop\\tasks\\words_sorted.txt", true, Encoding.ASCII);
+
+
+        for (int i=0; i< word.Length is ++)
+        {
+            char patternchar = pattern[i];
+            char wordsch = word[i]
+            words.Read(i);
+
+        }if (patternchar != '*')
+        {
+
+        if (patternchar != wordsch)
+        {
+            return false;
+        }
+            else
+            {
+                if patternchar[i].containt(words);
+
+
+            }
+
+            
+       
+        
+
+    }
+
+    public static void Main(string[] args)
+    {
+       
+        StreamReader words = new StreamReader("C:\\Users\\BilelUni5\\Desktop\\tasks\\words_sorted.txt");
+        string word = words.ReadLine(); 
+
+        string pattern = "***dg*";
+
+            List<string> word = matchwords(word, pattern);
+
+            Console.WriteLine(words);
+            foreach (string pattern in words)
+            {
+                Console.WriteLine(word);
+            }
+
+    }
+
+
+}
